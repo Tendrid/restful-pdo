9f7bc5bafced49a0d3b7373aed44e1892b428846
Tendrid <tendrid@gmail.com>
2014-04-02 14:54:21 -0400

    adding settings loader and PDO connect

--- api.php
+++ api.php
@@ -1,4 +1,62 @@
-<?php
+<?php namespace restpdo;
 // restful interface.
+use \PDO;
+
+error_reporting(E_ALL);
+ini_set('display_errors',16384);
+
+class API{
+
+    protected $config; 
+
+    function __construct(){
+        $this->loadSettings();
+        $this->_connect();
+
+    }
+
+    function loadSettings($path="./settings.php"){
+        $required = Array('dsn','user','password');
+        $optional = Array();
+        require($path);
+
+        foreach($required as $key){
+            if(!isset($$key)){
+                throw("required config value [$key] missing");
+            }else{
+                $this->config[$key] = $$key;
+            }
+        }
+        foreach($optional as $key){
+            if(!isset($$key)){
+                $this->config[$key] = NULL;
+            }else{
+                $this->config[$key] = $$key;
+            }
+        }
+
+    }
+
+    function _connect(){
+        try {
+            $dbh = new PDO($this->config['dsn'], $this->config['user'], $this->config['password']);
+        } catch (PDOException $e) {
+            $this->error($e);
+        }        
+    }
+
+
+    function error($e){
+
+    }
+}
+
+
+
+// if we're calling the file directly, handle the restful request
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) {
+    $API = new API();
+}
+
 
 ?>
--- settings_default.php
+++ settings_default.php
@@ -1,4 +1,9 @@
 <?php
 // default settings file
 // copy this file to ./settings.php and alter the settings as needed
+
+$dsn = 'mysql:dbname=models;host=localhost';
+$user = 'dbuser';
+$password = 'dbpass';
+
 ?>
